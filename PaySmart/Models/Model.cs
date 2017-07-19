0c8c1bbf09420f28a50a5ef26db1494839965bc8
prasanthkumar512 <jaligapu.prashanth512@gmail.com>
2017-07-19 11:28:31 +0530

    Addedticket booking api

--- PaySmart/Models/Model.cs
+++ PaySmart/Models/Model.cs
@@ -388,6 +388,83 @@ namespace Paysmart.Models
 
     }
 
+    public class BookingDetails
+    {
+        public int Id { get; set; }
+        public string TicketNo { get; set; }
+        public string TransId { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNo { get; set; }
+        public string AltMobileNo { get; set; }
+        public string Address { get; set; }
+        public DateTime? JourneyDate { get; set; }
+        public DateTime? JourneyTime { get; set; }
+        public decimal perunitprice { get; set; }
+        public DateTime? ArrivalDate { get; set; }
+        public DateTime? ArrivalTime { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public DateTime? DepartureTime { get; set; }
+        public string Src { get; set; }
+        public string Dest { get; set; }
+        public int SrcId { get; set; }
+        public int DestId { get; set; }
+        public int RouteId { get; set; }
+        public int VehicleId { get; set; }
+        public int NoOfSeats { get; set; }
+        public string Seats { get; set; }
+        public string Status { get; set; }
+        public int StatusId { get; set; }
+        public string BookedBy { get; set; }
+        public int BookedById { get; set; }
+        public decimal Amount { get; set; }
+        public string BookingType { get; set; }
+        public int BookingTypeId { get; set; }
+        public string JourneyType { get; set; }
+        public int JourneyTypeId { get; set; }
+        public int UserId { get; set; }
+        public string insupddelflag { get; set; }
+
+        public IEnumerable<BookedSeats> BookedSeats { get; set; }
+
+    }
+    public class BookedSeats
+    {
+        public int Id { get; set; }
+        public int BookingId { get; set; }
+        public string TicketNo { get; set; }
+        public string SeatNo { get; set; }
+        public int SeatId { get; set; }
+        public int VehicleId { get; set; }
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public DateTime JourneyDate { get; set; }
+        public DateTime JourneyTime { get; set; }
+        public string Status { get; set; }
+        public int StatusId { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+        public string IdProof { get; set; }
+        public string PassengerType { get; set; }
+        public int PrimaryPassenger { get; set; }
+
+        public string insupddelflag { get; set; }
+    }
+    public class BookedTicketDetails
+    {
+        public int Id { get; set; }
+        public int BookingId { get; set; }
+        public string TicketNo { get; set; }
+        public string TransId { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNo { get; set; }
+        public string TicketContent { get; set; }
+        public string insupddelflag { get; set; }
+    }
+
 }
 
 
